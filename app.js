1d21aa66155f2353a441bde7a3dc654bdef28c5e
Marcin Bejger <marcin.bejger@gmail.com>
2022-11-25 20:36:56 +0100

    new vue methods added

--- app.js
+++ app.js
@@ -10,11 +10,20 @@
     methods: {
         toggleShowBooks() {
             this.showBooks = !this.showBooks
-        }
+        },
         // changeTitle(title) {
         // //    this.title = "Harry Potter"
         // this.title = title
         // }
+        handleEvent(e, data) {
+            console.log(e, e.type)
+            if (data) {
+                console.log(data)
+            }
+        },
+        handleMousemove(e) {
+
+        }
     }
  })
 
